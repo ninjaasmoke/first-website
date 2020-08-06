d8680a8e3bd57d8bff2fcf9d9c93aca01bf234ac
Nithin Sai K J <nithins674@gmail.com>
2020-08-06 23:26:00 +0530

    Update script.js

--- site/module-4-assignment/js/script.js
+++ site/module-4-assignment/js/script.js
@@ -2,7 +2,7 @@
     function (window) {
         var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
         for (var name in names) {
-            if (names[name].charAt(0).toLowerCase() == 'j') {
+            if (names[name].charAt(0).toLowerCase() === 'j') {
                 byeSpeaker.speak(names[name]);
             } else {
                 helloSpeaker.speak(names[name]);
